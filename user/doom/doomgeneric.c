797ea5d1c192073d848dda1030125b708b981ead
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2022-10-15 00:00:35 -0400

    Notes and adjustments for the future

--- user/doom/doomgeneric.c
+++ user/doom/doomgeneric.c
@@ -2,6 +2,8 @@
 
 uint32_t* DG_ScreenBuffer = 0;
 
+// NOTE! called by i_main.c, which contains the main entry
+// After dg_Create returns, DoomMain is called and control transfers away from us
 
 void dg_Create()
 {
